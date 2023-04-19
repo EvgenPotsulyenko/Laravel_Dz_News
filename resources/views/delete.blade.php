abaf7f65655eb608c9ca84295d0898126a2b6481
Поцылуенко <83078431+EvgenPotsulyenko@users.noreply.github.com>
2023-04-19 14:56:25 +0300

    Add files via upload

---
+++ resources/views/delete.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>hello</title>
+    <meta charset="utf-8" />
+</head>
+<body>
+{{--http://127.0.0.1:8000/add--}}
+<form action="del" method="get">
+    <p>Header: <input type="text" name="Summary" /></p>
+    <button type="submit">Send</button>
+</form>
+</body>
+</html>
