720115b75af4b85e0338bf864eae508139e05ca9
Поцылуенко <83078431+EvgenPotsulyenko@users.noreply.github.com>
2023-04-19 14:57:35 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,9 @@
 <form action="crt" method="get">
     <button type="submit">Add New</button>
 </form><br>
+<form action="delView" method="get">
+    <button type="submit">delete</button>
+</form><br>
 @foreach($NewMods as $NewMod)
     <li><a  href= "{{url('open/'.$NewMod->Summary)}}">{{$NewMod->Summary}}</a></li><br>
     <li>{{$NewMod->Short_description}}</li><br>
